12927c706df4d70410cf4fb5b5e56433996aed5a
Jimena Martín Reina <jimena.martin.reina@alumnos.upm.es>
2025-05-22 13:43:29 +0200

    New folder parameter_handling created
    Signed-off-by: Jimena Martín Reina <jimena.martin.reina@alumnos.upm.es>

---
+++ parameter_handling/run_simulations.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+output_dir="OUTPUT_010225"
+mkdir -p "$output_dir"
+
+# Read each line of parameters.txt
+while read param1 param2 param3; do
+  # Create an unique name for the output file, and keep it in the created directory
+  output_file="${output_dir}/OutputFile_${param1}_${param2}_${param3}.txt"
+
+  # Execute the executable binary script and redirect the output to a file.
+  ./simulate_spacers1.2 "$param1" "$param2" "$param3" > "$output_file"
+
+  # Informative message the simulation was completed.
+  echo "Simulation completed with parameters (alpha, gamma and pEndemic): $param1 $param2 $param3. Saved in $output_file"
+  
+done < parameters.txt
+
+# Remember to give the shell script permission to execute: chmod +x run_simulations.sh
